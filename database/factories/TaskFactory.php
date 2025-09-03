2140b2e39c318fa086937bf6450aff92049bd86a
ghazi <zakariaghazi77@gmail.com>
2025-09-03 12:46:56 +0100

    first commit and creating database

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(),
+            'description' => fake()->paragraph(),
+            'status' => fake()->randomElement(['done', 'in_progress', 'pending']),
+            'user_id' => User::factory(), 
+        ];
+    }
+}
